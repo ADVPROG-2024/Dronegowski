80f1be722c1d0b2db4fdece1f7ce6fd131b28e1a
Sam Nejati <sam4nzd@gmail.com>
2024-12-01 19:52:30 +0100

    Cominciato a sviluppare il drone e creato la cartella per i tests

--- dronegowski/src/drone.rs
+++ dronegowski/src/drone.rs
@@ -1,26 +1,75 @@
+use crossbeam_channel::{select, select_biased, Receiver, Sender};
 use std::collections::HashMap;
-
-use crossbeam_channel::{Receiver, Sender};
 use wg_2024::controller::{DroneCommand, NodeEvent};
+use wg_2024::drone::{Drone, DroneOptions};
 use wg_2024::network::NodeId;
 use wg_2024::packet::Packet;
 
 #[derive(Debug, Clone)]
-pub struct DroneOptions {
+pub struct MyDrone {
     pub id: NodeId,
-    pub controller_send: Sender<NodeEvent>,
-    pub controller_recv: Receiver<DroneCommand>,
-    pub packet_recv: Receiver<Packet>,
-    pub packet_send: HashMap<NodeId, Sender<Packet>>,
-    pub pdr: f32,
+    pub sim_controller_send: Sender<NodeEvent>, // Canale per inviare eventi dal SC
+    pub sim_controller_recv: Receiver<DroneCommand>, // Canale per ricevere comandi dal SC
+    pub packet_recv: Receiver<Packet>,          // Canale per ricevere pacchetti
+    pub packet_send: HashMap<NodeId, Sender<Packet>>, // Mappa dei canali per inviare pacchetti ai neighbours nodes
+    pub pdr: f32,                                     // PDR
 }
 
-/// This is the drone interface.
-pub trait Drone {
-    /// The list packet_send would be crated empty inside new.
-    /// Other nodes are added by sending command
-    /// using the simulation control channel to send 'Command(AddChannel(...))'.
-    fn new(options: DroneOptions) -> Self;
+impl Drone for MyDrone {
+    fn new(options: DroneOptions) -> Self {
+        assert!(
+            !options.packet_send.contains_key(&options.id),
+            "neighbor with id {} which is the same as drone",
+            options.id
+        );
+        assert!(
+            !(options.pdr > 1.0 || options.pdr < 0.0),
+            "pdr out of bounds"
+        );
 
-    fn run(&mut self);
+        println!("Drone {} creato con PDR: {}", options.id, options.pdr);
+
+        Self {
+            id: options.id,
+            sim_controller_send: options.controller_send,
+            sim_controller_recv: options.controller_recv,
+            packet_recv: options.packet_recv,
+            pdr: options.pdr,
+            packet_send: HashMap::new(),
+        }
+    }
+
+    fn run(&mut self) {
+        println!("Drone {} in esecuzione...", self.id);
+        loop {
+            select! {
+                recv(self.packet_recv) -> packet_res => {
+                    if let Ok(packet) = packet_res {
+                        println!("Drone {}: Ricevuto un pacchetto {:?}", self.id, packet);
+                    } else {
+                        println!("Drone {}: Canale dei pacchetti chiuso", self.id);
+                        break;
+                    }
+                },
+                recv(self.sim_controller_recv) -> command_res => {
+                    if let Ok(command) = command_res {
+                        println!("Drone {}: Ricevuto un comando {:?}", self.id, command);
+                        if let DroneCommand::Crash = command {
+                            println!("Drone {}: Ricevuto comando di terminazione. Esco...", self.id);
+                            break;
+                        }
+                        if let DroneCommand::SetPacketDropRate(a) = command {
+                            self.pdr = a;
+                            println!("Drone {}: Aggiornato il PDR!", self.id);
+                        }
+                    } else {
+                        println!("Drone {}: Canale dei comandi chiuso", self.id);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
+
+impl MyDrone {}
