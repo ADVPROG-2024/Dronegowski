194a72b02d3ee8cee08081880503c14a9e160330
Sam Nejati <sam4nzd@gmail.com>
2024-12-03 15:24:40 +0100

    NodeEvent to DroneEvent

--- dronegowski/tests/common/mod.rs
+++ dronegowski/tests/common/mod.rs
@@ -3,17 +3,17 @@ use std::collections::HashMap;
 use crossbeam_channel::{Receiver, Sender};
 use dronegowski::MyDrone;
 use wg_2024::{
-    controller::{DroneCommand, NodeEvent},
+    controller::{DroneCommand, DroneEvent},
     packet::Packet,
 };
 
 pub fn default_drone() -> (
     MyDrone,
-    Receiver<NodeEvent>,
+    Receiver<DroneEvent>,
     Sender<DroneCommand>,
     Sender<Packet>,
 ) {
-    let (sim_controller_send, sim_controller_recv) = crossbeam_channel::unbounded::<NodeEvent>();
+    let (sim_controller_send, sim_controller_recv) = crossbeam_channel::unbounded::<DroneEvent>();
     let (command_send, command_recv) = crossbeam_channel::unbounded::<DroneCommand>();
     let (packet_send, packet_recv) = crossbeam_channel::unbounded::<Packet>();
 
